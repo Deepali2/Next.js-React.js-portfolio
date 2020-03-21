43fc8558509f82851259be92abf3725de67343c4
Deepali Garg <deepali.garg@zennioptical.com>
2020-03-21 14:54:38 -0700

    uses getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,31 @@
 import Link from 'next/link';
-import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
+import Layout from '../comps/Layout';
+import { Component } from 'react';
 
-const About = () => (
-  <Layout title='About'>
-    <Link href='/'><a>Go Home</a></Link>
-    <p>A Javascript Programmer</p>
-    <img
-      src='/Javascript_badge.png'
-      alt='javascript logo'
-      height='200px'
-    />
-  </Layout>
-)
+export default class About extends Component {
 
-export default About;
+  // getInitialProps is static so it can be accessed outside of the component as About.getInitialProps
+  static async getInitialProps() {
+    const res = await fetch('https://api.github.com/users/deepali2');
+    const data = await res.json();
+    return { user: data }
+  }
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <Layout title='About'>
+        <p>{user.name}</p>
+        <Link href='/'><a>Go Home</a></Link>
+        <p>A Javascript Programmer</p>
+        <img
+          src={user.avatar_url}
+          alt='Me'
+          height='200px'
+        />
+      </Layout>
+    )
+  }
+}
